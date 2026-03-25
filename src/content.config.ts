02b0b6609bbe90bb99cd84cf2f5a1b6e18c2f9ad
rathermercurial <rathermercurial@protonmail.com>
2026-03-25 00:24:57 -0700

    fix: hardcode R2_BUCKET_URL in content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,9 @@
 import { defineCollection } from "astro:content";
-import { loadEnv } from "vite";
 import { r2KnowledgeLoader } from "./loaders/r2-knowledge-loader";
-
-const env = loadEnv("", process.cwd(), "");
-const R2_BUCKET_URL = env.R2_BUCKET_URL;
-
-if (!R2_BUCKET_URL) {
-  throw new Error("R2_BUCKET_URL environment variable is not set. Create a .env file with R2_BUCKET_URL=https://knowledge-bucket.superbenefit.dev");
-}
 
 const knowledge = defineCollection({
   loader: r2KnowledgeLoader({
-    bucketUrl: R2_BUCKET_URL,
+    bucketUrl: "https://knowledge-bucket.superbenefit.dev",
   }),
 });
 
